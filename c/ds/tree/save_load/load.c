b4a3a11956c7470611704d4871eb9bfd67d56cbf
root <root@localhost.localdomain>
2023-03-03 15:24:52 -0500

    使用广义表来表示树形结构

---
+++ c/ds/tree/save_load/load.c
@@ -0,0 +1,149 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define FILENAME	"/tmp/out"
+
+
+struct treenode_st
+{
+	int data;
+	struct treenode_st *leftChild;
+	struct treenode_st *rightChild;
+};
+
+
+/**
+ * 插入树的节点,
+ * 插入规则是,比较新节点的id和当前节点的id,如果新节点的id > 当前节点的id,
+ * 则插入为当前节点的右孩子,其他情况为左孩子
+ */
+int insert(struct treenode_st **root, int data)
+{
+	struct treenode_st *newNode;
+	if(*root== NULL)
+	{
+		newNode = malloc(sizeof(*newNode));
+		newNode->data = data;
+		newNode->leftChild = NULL;
+		newNode->rightChild = NULL;
+
+		*root= newNode;
+		return 0;
+	}
+
+	if(data > (*root)->data)
+	{
+		insert (&((*root)->rightChild), data);
+	}
+	else{
+		insert(&(*root)->leftChild, data);
+	}
+
+	return 0;
+}
+
+
+void draw_(struct treenode_st *root, int level)
+{
+	int i;
+	if(root == NULL)	
+		return;
+
+	draw_(root->rightChild, level+1);
+	for(i = 0; i < level; i++)
+	{
+		printf("    ");
+	}
+	//print_s(&root->data);
+	printf("%c\n", root->data);
+	draw_(root->leftChild, level+1);
+}
+
+void draw(struct treenode_st *root)
+{
+	printf("\n");
+	printf("--------------------------------------\n");
+	draw_(root, 0);
+	printf("--------------------------------------\n");
+	printf("\n");
+}
+
+int load_(struct treenode_st **root, FILE *fp)
+{
+	char letter;
+	struct treenode_st *node = NULL;
+
+	letter = fgetc(fp);
+	//printf("letter=%c\n", letter);
+	if(letter != '(')
+	{
+		fprintf(stderr, "error format of the file. it should start with '('\n");
+		return -1;
+	}
+
+	letter = fgetc(fp);
+
+	if(letter == ')')
+	{
+		return 0;
+	}
+
+	node = malloc(sizeof(*node));
+	node->data = letter;
+	node->leftChild = NULL;
+	node->rightChild = NULL;
+
+	*root = node;
+	
+	load_(&node->leftChild, fp);
+	load_(&node->rightChild, fp);
+
+	
+	letter = fgetc(fp);
+	if(letter != ')')
+	{
+		fprintf(stderr, "error format of the file. it should end with ')'\n");
+		return -2;
+	}
+
+
+
+
+
+
+	
+}
+
+
+void load(struct treenode_st **root)
+{
+	FILE *fp = NULL;
+	fp = fopen(FILENAME, "r");
+	load_(root, fp);
+	fclose(fp);
+	
+}
+
+
+int main()
+{
+	struct treenode_st *root = NULL;
+	/*
+	int i;
+
+	char arr[] = "cefadjbh";
+
+	for(i = 0; i < strlen(arr); i++)
+	{
+		insert(&root, arr[i]);
+	}
+	*/
+	load(&root);
+
+	draw(root);
+
+
+
+	return 0;
+}
